cab6ae268ac8ace793e133ddebe320772c4a6635
zhangfei <zhangfei19841004@163.com>
2019-07-14 15:16:16 +0800

    initial

--- src/main/java/com/zf/executor/ExecutorCenter.java
+++ src/main/java/com/zf/executor/ExecutorCenter.java
@@ -1,16 +1,15 @@
 package com.zf.executor;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.zf.message.MessageType;
-import com.zf.service.WebSocketInfo;
+import com.zf.pojo.ExecutorToClientInfo;
 import com.zf.service.WebSocketServer;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedDeque;
 
 /**
  * Created by zhangfei on 2018/10/31.
@@ -18,21 +17,22 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 @Service
 public class ExecutorCenter {
 
-	public static ConcurrentHashMap<Integer, ExecutorInfo> ALL_EXECUTOR = new ConcurrentHashMap<>();
+    public static ConcurrentHashMap<Integer, ExecutorInfo> ALL_EXECUTOR = new ConcurrentHashMap<>();
 
-	public static ConcurrentHashMap<Integer, List<String>> EXECUTE_LOGS = new ConcurrentHashMap();
+    public static ConcurrentHashMap<Integer, List<String>> EXECUTE_LOGS = new ConcurrentHashMap();
 
-	public static ConcurrentHashMap<Integer, List<WebSocketServer>> LOOKING_CLIENTS = new ConcurrentHashMap();
+    public static ConcurrentHashMap<Integer, List<WebSocketServer>> LOOKING_CLIENTS = new ConcurrentHashMap();
 
-	public void executor(ExecutorInfo info) {
-		try {
-			JSONObject json = new JSONObject();
-			json.put("command", info.getExecuteCommand());
-			json.put("args", info.getExecuteArgs());
-			json.put("executeId", info.getExecuteId());
-			WebSocketServer.sendInfo(info.getCid(), info.getSessionId(), MessageType.COMMAND, JSON.toJSONString(json));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public void executor(ExecutorInfo info) {
+        try {
+            ExecutorToClientInfo toClientInfo = new ExecutorToClientInfo();
+            BeanUtils.copyProperties(info, toClientInfo);
+            toClientInfo.setExecuteId(info.getExecuteId());
+            toClientInfo.setCommand(info.getExecuteCommand());
+            toClientInfo.setArgs(info.getExecuteArgs());
+            WebSocketServer.sendInfo(info.getCid(), info.getSessionId(), MessageType.COMMAND, JSON.toJSONString(toClientInfo));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/com/zf/pojo/ExecutorToClientInfo.java
@@ -0,0 +1,111 @@
+package com.zf.pojo;
+
+/**
+ * Created by zhangfei on 2018/11/4.
+ */
+public class ExecutorToClientInfo {
+
+    private String command;
+
+    private String args;
+
+    private int executeId;
+
+    private String beforeFromServerPath;
+    private String beforeToClientPath;
+    private String beforeFromClientPath;
+    private String beforeToServerPath;
+
+    private String afterFromServerPath;
+    private String afterToClientPath;
+    private String afterFromClientPath;
+    private String afterToServerPath;
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String getArgs() {
+        return args;
+    }
+
+    public void setArgs(String args) {
+        this.args = args;
+    }
+
+    public int getExecuteId() {
+        return executeId;
+    }
+
+    public void setExecuteId(int executeId) {
+        this.executeId = executeId;
+    }
+
+    public String getBeforeFromServerPath() {
+        return beforeFromServerPath;
+    }
+
+    public void setBeforeFromServerPath(String beforeFromServerPath) {
+        this.beforeFromServerPath = beforeFromServerPath;
+    }
+
+    public String getBeforeToClientPath() {
+        return beforeToClientPath;
+    }
+
+    public void setBeforeToClientPath(String beforeToClientPath) {
+        this.beforeToClientPath = beforeToClientPath;
+    }
+
+    public String getBeforeFromClientPath() {
+        return beforeFromClientPath;
+    }
+
+    public void setBeforeFromClientPath(String beforeFromClientPath) {
+        this.beforeFromClientPath = beforeFromClientPath;
+    }
+
+    public String getBeforeToServerPath() {
+        return beforeToServerPath;
+    }
+
+    public void setBeforeToServerPath(String beforeToServerPath) {
+        this.beforeToServerPath = beforeToServerPath;
+    }
+
+    public String getAfterFromServerPath() {
+        return afterFromServerPath;
+    }
+
+    public void setAfterFromServerPath(String afterFromServerPath) {
+        this.afterFromServerPath = afterFromServerPath;
+    }
+
+    public String getAfterToClientPath() {
+        return afterToClientPath;
+    }
+
+    public void setAfterToClientPath(String afterToClientPath) {
+        this.afterToClientPath = afterToClientPath;
+    }
+
+    public String getAfterFromClientPath() {
+        return afterFromClientPath;
+    }
+
+    public void setAfterFromClientPath(String afterFromClientPath) {
+        this.afterFromClientPath = afterFromClientPath;
+    }
+
+    public String getAfterToServerPath() {
+        return afterToServerPath;
+    }
+
+    public void setAfterToServerPath(String afterToServerPath) {
+        this.afterToServerPath = afterToServerPath;
+    }
+}
