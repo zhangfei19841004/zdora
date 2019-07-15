f359d804d673568016f5e4a9e404084e0d44af31
zhangfei <zf@112633>
2019-07-15 19:27:57 +0800

    initial

--- src/main/java/com/zf/zdora/executor/ExecutorHandler.java
+++ src/main/java/com/zf/zdora/executor/ExecutorHandler.java
@@ -9,57 +9,97 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.stream.Collectors;
 
 /**
  * Created by zhangfei on 2019/05/31.
  */
 public class ExecutorHandler extends Thread {
 
-    private static Logger logger = LoggerFactory.getLogger(ExecutorHandler.class);
+	private static Logger logger = LoggerFactory.getLogger(ExecutorHandler.class);
 
-    private ZdoraClient zdoraClient;
+	private ZdoraClient zdoraClient;
 
-    public void setZdoraClient(ZdoraClient zdoraClient) {
-        this.zdoraClient = zdoraClient;
-    }
+	public void setZdoraClient(ZdoraClient zdoraClient) {
+		this.zdoraClient = zdoraClient;
+	}
 
-    private ExecutorFromServerInfo fromServerInfo;
+	private ExecutorFromServerInfo fromServerInfo;
 
-    public void setFromServerInfo(ExecutorFromServerInfo fromServerInfo) {
-        this.fromServerInfo = fromServerInfo;
-    }
+	public void setFromServerInfo(ExecutorFromServerInfo fromServerInfo) {
+		this.fromServerInfo = fromServerInfo;
+	}
 
-    @Override
-    public void run() {
-        ExecutorClientInfo begin = new ExecutorClientInfo();
-        begin.setType(2);
-        begin.setExecuteId(fromServerInfo.getExecuteId());
-        begin.setExecuteStatus(ExecutorStatus.STATUS2.getStatus());
-        if (StringUtils.isNotBlank(fromServerInfo.getBeforeFromClientPath())) {
-            List<String> paths = new ArrayList<>();
-            FileUtils.getAllFilePaths(paths, fromServerInfo.getBeforeFromClientPath());
-            for (String path : paths) {
-                //HttpClientUtils.upload(path, targetPath, "http://" + Run.serverHost + ":" + Run.serverPort + "/upload");
-            }
-        }
-        begin.setMessage("开始执行: " + fromServerInfo.getCommand() + " " + fromServerInfo.getArgs());
-        zdoraClient.send(begin.toString());
-        CommandUtil.executeCommand(zdoraClient, fromServerInfo.getCommand(), fromServerInfo.getArgs(), fromServerInfo.getExecuteId());
-        logger.info("执行完命令: " + fromServerInfo.getCommand() + " " + fromServerInfo.getArgs());
-        ExecutorClientInfo end = new ExecutorClientInfo();
-        end.setType(2);
-        end.setExecuteId(fromServerInfo.getExecuteId());
-        end.setExecuteStatus(ExecutorStatus.STATUS3.getStatus());
-        end.setMessage("执行完成: " + fromServerInfo.getCommand() + " " + fromServerInfo.getArgs());
-        zdoraClient.send(end.toString());
-    }
+	@Override
+	public void run() {
+		ExecutorClientInfo begin = new ExecutorClientInfo();
+		begin.setType(2);
+		begin.setExecuteId(fromServerInfo.getExecuteId());
+		begin.setExecuteStatus(ExecutorStatus.STATUS2.getStatus());
+		if (StringUtils.isNotBlank(fromServerInfo.getBeforeFromClientPath())) {
+			this.uploadFromClientToServer(fromServerInfo.getBeforeFromClientPath(), fromServerInfo.getBeforeToServerPath());
+		}
+		begin.setMessage("拷贝文件: 从客户端" + fromServerInfo.getBeforeFromClientPath() + " 至服务端" + fromServerInfo.getBeforeToServerPath());
+		zdoraClient.send(begin.toString());
+		begin.setMessage("开始执行: " + fromServerInfo.getCommand() + " " + fromServerInfo.getArgs());
+		zdoraClient.send(begin.toString());
+		CommandUtil.executeCommand(zdoraClient, fromServerInfo.getCommand(), fromServerInfo.getArgs(), fromServerInfo.getExecuteId());
+		logger.info("执行完命令: " + fromServerInfo.getCommand() + " " + fromServerInfo.getArgs());
+		if (StringUtils.isNotBlank(fromServerInfo.getAfterFromClientPath())) {
+			this.uploadFromClientToServer(fromServerInfo.getAfterFromClientPath(), fromServerInfo.getAfterToServerPath());
+		}
+		ExecutorClientInfo end = new ExecutorClientInfo();
+		end.setType(2);
+		end.setExecuteId(fromServerInfo.getExecuteId());
+		end.setExecuteStatus(ExecutorStatus.STATUS2.getStatus());
+		end.setMessage("执行完成: " + fromServerInfo.getCommand() + " " + fromServerInfo.getArgs());
+		zdoraClient.send(end.toString());
+		end.setExecuteStatus(ExecutorStatus.STATUS2.getStatus());
+		end.setMessage("拷贝文件: 从客户端" + fromServerInfo.getAfterFromClientPath() + " 至服务端" + fromServerInfo.getAfterToServerPath());
+		zdoraClient.send(end.toString());
 
-    public static void main(String[] args) throws Exception {
-        String path = "/Users/zhangfei/Desktop/zdora-client";
-        List<String> paths = new ArrayList<>();
-        FileUtils.getAllFilePaths(paths, path);
-        paths.forEach(System.out::println);
-    }
+		end.setExecuteStatus(ExecutorStatus.STATUS3.getStatus());
+		end.setMessage("执行结束!");
+		zdoraClient.send(end.toString());
+	}
+
+	private void uploadFromClientToServer(String clientPath, String serverPath) {
+		if (StringUtils.isNotBlank(clientPath)) {
+			List<String> paths = new ArrayList<>();
+			FileUtils.getAllFilePaths(paths, clientPath);
+			for (String path : paths) {
+				List<String> targetPath = this.getTargetPath(path, clientPath, serverPath);
+				HttpClientUtils.upload(path, targetPath, "http://" + Run.serverHost + ":" + Run.serverPort + "/upload");
+			}
+		}
+	}
+
+	private List<String> getTargetPath(String path, String clientRootPath, String serverRootPath) {
+		List<String> list = new ArrayList<>();
+		if (StringUtils.isNotBlank(serverRootPath)) {
+			File serverRootFile = new File(serverRootPath);
+			list = Arrays.stream(serverRootFile.getPath().split(Matcher.quoteReplacement(File.separator))).filter(StringUtils::isNotBlank).collect(Collectors.toList());
+		}
+		File root = new File(clientRootPath);
+		clientRootPath = root.getAbsolutePath();
+		path = path.substring(0, path.lastIndexOf(File.separator));
+		path = path.substring(clientRootPath.length());
+		if (StringUtils.isBlank(path) || path.length() == 1) {
+			return list;
+		}
+		path = path.substring(1);
+		list.addAll(Arrays.asList(path.split(Matcher.quoteReplacement(File.separator))));
+		return list;
+	}
+
+	public static void main(String[] args) throws Exception {
+		String path = "";
+		File f = new File(path);
+		Arrays.stream(f.getPath().split(Matcher.quoteReplacement(File.separator))).filter(StringUtils::isNotBlank).forEach(System.out::println);
+	}
 }
