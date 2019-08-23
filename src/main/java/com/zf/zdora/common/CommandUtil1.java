094f0e37242653b088d20e3974b06bea38a93e47
zhangfei <zhangfei19841004@163.com>
2019-08-23 23:15:06 +0800

    initial

--- src/main/java/com/zf/zdora/common/CommandUtil1.java
+++ src/main/java/com/zf/zdora/common/CommandUtil1.java
@@ -2,22 +2,29 @@ package com.zf.zdora.common;
 
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
-import org.apache.commons.exec.LogOutputStream;
 import org.apache.commons.exec.PumpStreamHandler;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
 
 public class CommandUtil1 {
 
+	private static final String os = System.getProperty("os.name");
+
+	private static String charset = "UTF-8";
+
 	public static void executeCommand() {
 		try {
-			PumpStreamHandler streamHandler = new PumpStreamHandler(new CollectingLogOutputStream());
 			CommandLine commandline = new CommandLine("E:\\workspace\\zdora-test\\run.bat");
 			commandline.addArgument("123456789");
 			DefaultExecutor exec = new DefaultExecutor();
 			exec.setExitValues(null);
-
+			if(os.toLowerCase().startsWith("win") && StringUtils.isNotBlank(commandline.getExecutable()) &&
+					(commandline.getExecutable().toLowerCase().endsWith("cmd") ||
+							commandline.getExecutable().toLowerCase().endsWith("bat"))){
+				charset = "GBK";
+			}
+			PumpStreamHandler streamHandler = new PumpStreamHandler(new CollectingLogOutputStream(CommandUtil1.charset));
 			exec.setStreamHandler(streamHandler);
 			exec.execute(commandline);// exit code: 0=success, 1=error
 
@@ -28,15 +35,20 @@ public class CommandUtil1 {
 
 	public static class CollectingLogOutputStream extends LogOutputStream {
 
+		public CollectingLogOutputStream(String charset) {
+			super(charset);
+		}
+
 		@Override
 		protected void processLine(String line, int level) {
-			System.out.println(new String(new String(line.getBytes(Charset.forName("utf-8"))).getBytes(Charset.forName("GBK"))));
+			System.out.println(line);
 		}
 
 	}
 
 	public static void main(String[] args) {
-		CommandUtil1.executeCommand();
+		//CommandUtil1.executeCommand();
+		System.out.println(System.getProperty("os.name"));
 	}
 
 }
